36b19e7f15975b8ac3e6102865e433f513648558
Pavan Kumar <pavan082005@gmail.com>
2025-03-18 23:08:59 +0530

    sold api done

--- src/lib/cosmic.js
+++ src/lib/cosmic.js
@@ -25,14 +25,13 @@ export async function getDataByCategory(id) {
   }
 }
 
-
 export async function getAllDataByType(dataType = 'categories') {
   try {
     const data = await cosmic.objects
       .find({ type: dataType })
       .props('title,slug,id,metadata')
       .depth(1)
-      console.log(data);
+    console.log(data)
     return data.objects
   } catch (error) {
     // Don't throw if an slug doesn't exist
@@ -50,8 +49,8 @@ export async function getDataBySlug(slug) {
       })
       .props('title,slug,id,metadata,created_at')
       .depth(1)
-     // console.log(data.objects[0].metadata.seller);
-      
+    // console.log(data.objects[0].metadata.seller);
+
     return data.objects
   } catch (error) {
     // Don't throw if an slug doesn't exist
@@ -68,11 +67,18 @@ export async function getDataByRoll(roll) {
   try {
     const data = await cosmic.objects
       .find(query)
-      .props('title,metadata.image')
+      .props('title,metadata.image,metadata.seller')
       .depth(1)
-    return data.objects
+    console.log(
+      data.objects.filter(
+        n => n.metadata?.seller?.toLowerCase() === roll.toLowerCase()
+      )
+    )
+    return data.objects.filter(
+      n => n.metadata?.seller?.toLowerCase() === roll.toLowerCase()
+    )
   } catch (error) {
-    console.log("custom error" , error);
+    console.log('custom error', error)
     if (is404(error)) return
     throw error
   }
